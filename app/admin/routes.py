ff54a9be724c4042e320d29ea9912678b67379a6
schaak722 <js@schaak.mt>
2026-02-03 12:31:49 +0100

    Update routes.py

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,3 +1,5 @@
+import os
+from flask import abort
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_required, current_user
 from sqlalchemy import text
