c7afb12db2f5c58c8f2d7893de2dc140cda4b7c9
schaak722 <js@schaak.mt>
2026-02-05 17:28:52 +0100

    Update __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,1 +1,10 @@
+from flask import Blueprint
+
+routes_bp = Blueprint("routes", __name__)
+
+from .dashboard import *  # noqa
+from .items import *      # noqa
+from .sales import *      # noqa
+from .imports import *    # noqa
+from .audit import *      # noqa
 
