d6bb7c5b0a0174a46dc84d1beee34e431201b52f
schaak722 <js@schaak.mt>
2026-02-05 19:08:01 +0100

    Update extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -5,5 +5,6 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 migrate = Migrate()
 login_manager = LoginManager()
-login_manager.login_view = "auth.login"
+login_manager.login_view = "auth.login_page"
+
 
