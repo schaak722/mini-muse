35997cdf399b2bd83e1fe0a41d1724e27a34925b
schaak722 <js@schaak.mt>
2026-02-05 16:47:27 +0100

    Update __init__.py

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,1 +1,9 @@
+from flask import Blueprint
+
+api_bp = Blueprint("api", __name__)
+
+from .items import *          # noqa
+from .sales import *          # noqa
+from .import_batches import * # noqa
+from .audit_logs import *     # noqa
 
